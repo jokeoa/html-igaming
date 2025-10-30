f478dbe4d138c0e9b686a8ccdc57e56ce1984ed3
BekdauletBol <bekdauletmakemusic@gmail.com>
2025-10-30 10:06:01 +0500

    add jQuery on task-4

--- src/app/catalog/script.js
+++ src/app/catalog/script.js
@@ -4,3 +4,28 @@ $(window).on('scroll', function() {
   const scrollPercent = (scrollTop / docHeight) * 100;
   $('#scroll-progress').css('width', scrollPercent + '%');
 });
+
+
+// Task 5: Animated Number Counter 
+let countersAnimated = false;
+
+$(window).on('scroll', function() {
+  if (countersAnimated) return;
+
+  const $section = $('.stats-section');
+  const sectionTop = $section.offset().top;
+  const sectionHeight = $section.outerHeight();
+  const windowHeight = $(window).height();
+  const scroll = $(window).scrollTop();
+
+  if (scroll > sectionTop + sectionHeight - windowHeight - 100) {
+    animateCounters();
+    countersAnimated = true;
+    
+    $(window).off('scroll', arguments.callee);
+  }
+});
+// jQuery
+$(document).ready(function() {
+  console.log("jQuery is ready!");
+});
